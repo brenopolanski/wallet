74aa9739e847b6ab8df11dcbd2618007a92aafa4
faustbrian <faustbrian@users.noreply.github.com>
2021-11-17 01:48:25 +0000

    style: resolve style guide violations

--- src/plugins/core/internals/plugin-permission.ts
+++ src/plugins/core/internals/plugin-permission.ts
@@ -11,31 +11,31 @@ type Rule<T = any> = (context: MiddlewareContext) => (result: T) => T | never;
 
 export const isServiceDefinedInConfig: Rule =
 	({ service, plugin }) =>
-		(result) => {
-			if (!!service && plugin.config().permissions()?.includes(service.id())) {
-				return result;
-			}
-			return console.error.bind(
-				console,
-				`The plugin ${plugin.config().name()} did not define ${service?.id()} its permissions.`,
-			);
-		};
+	(result) => {
+		if (!!service && plugin.config().permissions()?.includes(service.id())) {
+			return result;
+		}
+		return console.error.bind(
+			console,
+			`The plugin ${plugin.config().name()} did not define ${service?.id()} its permissions.`,
+		);
+	};
 
 // TODO:
 export const isServiceEnabled: Rule = () => (result) => result;
 
 export const isPluginEnabled: Rule =
 	({ profile, plugin }) =>
-		(result) => {
-			if (plugin.isEnabled(profile)) {
-				return result;
-			}
+	(result) => {
+		if (plugin.isEnabled(profile)) {
+			return result;
+		}
 
-			return console.error.bind(
-				console,
-				`The plugin ${plugin.config().name()} is not enabled by the current profile.`,
-			);
-		};
+		return console.error.bind(
+			console,
+			`The plugin ${plugin.config().name()} is not enabled by the current profile.`,
+		);
+	};
 
 export const applyPluginMiddlewares = (context: MiddlewareContext, rules: Rule[]) => (response: any) =>
 	rules.reduce((accumulator, rule) => rule(context)(accumulator), response);
