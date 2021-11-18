7191685d502d1625835ee019d9a182f4c84e3027
goga-m <goga.mamoulasvili@gmail.com>
2021-11-19 01:46:10 +0200

    wip

---
+++ src/domains/transaction/hooks/use-multisignature-registration.test.tsx
@@ -0,0 +1,32 @@
+import { Contracts, DTO } from "@payvo/sdk-profiles";
+import { env, getDefaultProfileId } from "utils/testing-library";
+import { formatMultiSignatureInputData } from "./use-multisignature-registration";
+
+describe("Use MultiSignature Registration Hook", () => {
+	let profile: Contracts.IProfile;
+	let wallet: Contracts.IReadWriteWallet;
+
+	beforeAll(async () => {
+		profile = env.profiles().findById(getDefaultProfileId());
+		wallet = profile.wallets().first();
+	});
+
+	it("should format transaction data based on multisignature type", () => {
+		const result = formatMultiSignatureInputData({ wallet, min: 2, publicKeys: ["1", "2"], mandatoryKeys: [] });
+
+		expect(result).toEqual({ publicKeys: ["1", "2"], min: 2 });
+
+		// Advanced type
+		jest.spyOn(wallet.network(), "multiSignatureType").mockReturnValue("advanced");
+		const advanced = formatMultiSignatureInputData({
+			wallet,
+			min: 2,
+			publicKeys: ["1", "2"],
+			mandatoryKeys: ["1"],
+		});
+
+		expect(advanced).toEqual({ mandatoryKeys: ["1"], numberOfSignatures: 2, optionalKeys: ["2"] });
+
+		jest.clearAllMocks();
+	});
+});
