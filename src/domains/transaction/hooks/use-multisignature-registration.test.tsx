64a65b7244679ac6538bef487850f1c2332e61d3
goga-m <goga-m@users.noreply.github.com>
2021-11-19 00:08:47 +0000

    style: resolve style guide violations

--- src/domains/transaction/hooks/use-multisignature-registration.test.tsx
+++ src/domains/transaction/hooks/use-multisignature-registration.test.tsx
@@ -1,5 +1,6 @@
-import { Contracts, DTO } from "@payvo/sdk-profiles";
+import { Contracts } from "@payvo/sdk-profiles";
 import { env, getDefaultProfileId } from "utils/testing-library";
+
 import { formatMultiSignatureInputData } from "./use-multisignature-registration";
 
 describe("Use MultiSignature Registration Hook", () => {
@@ -12,17 +13,17 @@ describe("Use MultiSignature Registration Hook", () => {
 	});
 
 	it("should format transaction data based on multisignature type", () => {
-		const result = formatMultiSignatureInputData({ wallet, min: 2, publicKeys: ["1", "2"], mandatoryKeys: [] });
+		const result = formatMultiSignatureInputData({ mandatoryKeys: [], min: 2, publicKeys: ["1", "2"], wallet });
 
-		expect(result).toStrictEqual({ publicKeys: ["1", "2"], min: 2 });
+		expect(result).toStrictEqual({ min: 2, publicKeys: ["1", "2"] });
 
 		// Advanced type
 		jest.spyOn(wallet.network(), "multiSignatureType").mockReturnValue("advanced");
 		const advanced = formatMultiSignatureInputData({
-			wallet,
+			mandatoryKeys: ["1"],
 			min: 2,
 			publicKeys: ["1", "2"],
-			mandatoryKeys: ["1"],
+			wallet,
 		});
 
 		expect(advanced).toStrictEqual({ mandatoryKeys: ["1"], numberOfSignatures: 2, optionalKeys: ["2"] });
